06ae99ae869a5abe2efb69befff548cf292ddcb9
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-10-31 17:09:15 +0200

    Sovellus toimii

--- src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
@@ -1,7 +1,17 @@
 package loppuprojekti24.loppuprojekti.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 public class TapahtumaController {
 
+@RequestMapping("/")
+@ResponseBody
+public String returnMessage() {
+    return "Kokeilen että toimii";
+}
 
 }
