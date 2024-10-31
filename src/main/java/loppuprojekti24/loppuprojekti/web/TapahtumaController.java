763b5baab0faf1a9bbce2917f2649ad6782d9f2d
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-10-31 17:16:04 +0200

    testattu, että Request Param toimii

--- src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
@@ -2,6 +2,7 @@ package loppuprojekti24.loppuprojekti.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -10,8 +11,19 @@ public class TapahtumaController {
 
 @RequestMapping("/")
 @ResponseBody
-public String returnMessage() {
-    return "Kokeilen että toimii";
+public String returnTapahtumat() {
+    return "Listataan tähän tapahtumat automaattisesti";
 }
 
+
+@RequestMapping("osallistujat")
+@ResponseBody
+public String returnOsallistujat(@RequestParam (name="nimesi") String etunimi) {
+    return "Hei " + etunimi;
 }
+
+
+// viimeinen loppusulku
+}
+
+
