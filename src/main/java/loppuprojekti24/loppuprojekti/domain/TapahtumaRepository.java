75fa620cc798fbaf6949469bf082e06dd8f60026
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-01 15:21:13 +0200

    lisätty tapahtuma -repository

---
+++ src/main/java/loppuprojekti24/loppuprojekti/domain/TapahtumaRepository.java
@@ -0,0 +1,11 @@
+package loppuprojekti24.loppuprojekti.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface TapahtumaRepository extends CrudRepository<Tapahtuma, Long>{
+    List<Tapahtuma> findByName(String nimi);
+
+}
