f166520a16375bfe2ab83dfd39afc3e60e478f5a
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-02 11:44:46 +0200

    nimetty endpointit uusiksi. Muokattu PostMapping tallennus

--- src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
@@ -17,30 +17,32 @@ public class TapahtumaController {
 @Autowired
 private TapahtumaRepository tapahtumaRepository;
 
-//hakee tapahtuman lisäyslomakkeen
-//GET-metodi näyttää lomakkeen luomalla uuden Tapahtuma-objektin
-@GetMapping("/lisaaTapahtuma")
-public String addTapahtuma(Model model) {
-     model.addAttribute("tapahtuma", new Tapahtuma ());
-    return "tapahtumaLomake";
-}
-
-
-// avaa yksittäisen tapahtuman, muokkaa polkua myöhemmin
-//POST-metodi tallentaa lomakkeen tiedot ja näyttää ne seuraavassa näkymässä.
-@PostMapping("/tapahtumaid")
-	public String submitTapahtuma(@ModelAttribute Tapahtuma tapahtuma, Model model) {
-		model.addAttribute("tapahtuma", tapahtuma);
-		return "tapahtuma";
-	}
-
-
 // hakee kaikki tapahtumat listaukseen
 @GetMapping ("/lista")
 public String getTapahtumat (Model model) {
     model.addAttribute("tapahtumat", tapahtumaRepository.findAll());
     return "tapahtumatLista";
 }
+
+
+//hakee tapahtuman lisäyslomakkeen
+//GET-metodi näyttää lomakkeen luomalla uuden Tapahtuma-objektin
+@GetMapping("/lomake")
+public String addTapahtuma(Model model) {
+     model.addAttribute("tapahtuma", new Tapahtuma ());
+    return "tapahtumaLomake";
+}
+
+
+// tallentaa yksittaisen tapahtuman
+//POST-metodi tallentaa lomakkeen tiedot ja näyttää ne seuraavassa näkymässä.
+@PostMapping("/lisaa")
+	public String submitTapahtuma(Tapahtuma tapahtuma) {
+		tapahtumaRepository.save(tapahtuma);
+		return "tapahtuma";
+	}
+
+
 
 
 // viimeinen loppusulku
