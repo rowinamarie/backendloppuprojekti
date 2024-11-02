3c09efc0e0ca25336430717d05361a6704d82dc4
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-02 17:06:30 +0200

    lisätty myös rest kaupungeille

--- src/main/java/loppuprojekti24/loppuprojekti/web/RestTapahtumaController.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/RestTapahtumaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import loppuprojekti24.loppuprojekti.domain.Kaupunki;
 import loppuprojekti24.loppuprojekti.domain.KaupunkiRepository;
 import loppuprojekti24.loppuprojekti.domain.Tapahtuma;
 import loppuprojekti24.loppuprojekti.domain.TapahtumaRepository;
@@ -20,6 +21,9 @@ public class RestTapahtumaController {
     
     @Autowired
     private TapahtumaRepository tapahtumaRepository;
+
+    @Autowired
+    private KaupunkiRepository kaupunkiRepository;
 
 // REST etsii kaikki tapahtumat
     @GetMapping("/tapahtumat")
@@ -31,8 +35,20 @@ public class RestTapahtumaController {
     @GetMapping("/tapahtumat/{id}")
     public @ResponseBody Optional<Tapahtuma> findTapahtumaRest(@PathVariable("id") Long tapahtumaId) {	
     	return tapahtumaRepository.findById(tapahtumaId);
-    }       
+    }   
     
+    
+// REST etsii kaikki kaupungit
+    @GetMapping("/kaupungit")
+    public @ResponseBody List<Kaupunki> kaupunkiListRest() {	
+        return (List<Kaupunki>) kaupunkiRepository.findAll();
+    } 
+    
+    // REST etsii kaupungin id:n perusteella
+    @GetMapping("/kaupungit/{id}")
+    public @ResponseBody Optional<Kaupunki> findKaupunkiRest(@PathVariable("id") Long kaupunkiid) {	
+    	return kaupunkiRepository.findById(kaupunkiid);
+    } 
 
 
 }
