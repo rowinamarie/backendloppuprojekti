f109335b091d5d1939f41b10298271d5b2326fb9
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-02 15:52:13 +0200

    poistettu turhia importteja

--- src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/TapahtumaController.java
@@ -3,12 +3,9 @@ package loppuprojekti24.loppuprojekti.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import loppuprojekti24.loppuprojekti.domain.Tapahtuma;
 import loppuprojekti24.loppuprojekti.domain.TapahtumaRepository;
