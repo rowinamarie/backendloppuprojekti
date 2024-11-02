474220b64e8738bd572d213343f5570a4acffd51
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-02 12:05:54 +0200

    lisätty testidatoihin päivämäärät

--- src/main/java/loppuprojekti24/loppuprojekti/LoppuprojektiApplication.java
+++ src/main/java/loppuprojekti24/loppuprojekti/LoppuprojektiApplication.java
@@ -1,4 +1,6 @@
 package loppuprojekti24.loppuprojekti;
+import java.time.LocalDate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;	
 import org.springframework.boot.CommandLineRunner;
@@ -22,10 +24,10 @@ public class LoppuprojektiApplication {
 	public CommandLineRunner tapahtumaDemo(TapahtumaRepository tapahtumaRepository) {
 		return (args) -> {
 			log.info("Lisätään testitapahtumia");
-			tapahtumaRepository.save(new Tapahtuma( "Konsertti", "Helsinki", "uusitapahtuma"));
-			tapahtumaRepository.save(new Tapahtuma("Juhlat", "Tampere", "koko perheen tapahtum"));	
+			tapahtumaRepository.save(new Tapahtuma( "Konsertti", "Helsinki", "uusitapahtuma",LocalDate.of(2024, 5, 20) ));
+			tapahtumaRepository.save(new Tapahtuma("Juhlat", "Tampere", "koko perheen tapahtum", LocalDate.of(2024, 6, 15)));	
 			
-			tapahtumaRepository.save(new Tapahtuma("Illallinen", "Espoo", "3 ruokalajin illallinen"));
+			tapahtumaRepository.save(new Tapahtuma("Illallinen", "Espoo", "3 ruokalajin illallinen", LocalDate.of(2024, 7, 10)));
 			
 			log.info("Hakee kaikki tapahtumat");
 			for (Tapahtuma tapahtuma : tapahtumaRepository.findAll()) {
