e8bfba3ca15bd4e4b967ef323c23a5019cad948c
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-03 12:22:33 +0200

    muokattu käyttämään käyttäserviceä

--- src/main/java/loppuprojekti24/loppuprojekti/web/WebSecurityConfig.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/WebSecurityConfig.java
@@ -1,20 +1,19 @@
 package loppuprojekti24.loppuprojekti.web;
+
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,40 +21,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
+    @Autowired
+    private KayttajaServiceImpl kayttajaDetailsService;
+
     @Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers(antMatcher("/css/**")).permitAll() // Enable css when logged out
-				.anyRequest().authenticated()
-			).formLogin(formlogin -> formlogin
-				.defaultSuccessUrl("/lista", true).permitAll()
-			).logout(logout -> logout
-				.permitAll()
-			);
-		return http.build();
-	}
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(antMatcher("/css/**")).permitAll() // Enable css when logged out
+                        .requestMatchers(HttpMethod.DELETE, "/delete/**").hasAuthority("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(formlogin -> formlogin
+                        .defaultSuccessUrl("/lista", true).permitAll())
+                .logout(logout -> logout
+                        .permitAll());
+        return http.build();
+    }
 
-
-    	@Bean
-	public UserDetailsService userDetailsService() {
-		
-		System.out.println("in-memory users - luodaan näitä kaksi kappaletta");
-		List<UserDetails> users = new ArrayList<UserDetails>();
-
-		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-		UserDetails user1 = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
-
-		users.add(user1);
-
-		UserDetails user2 = User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER", "ADMIN")
-				.build();
-
-		users.add(user2);
-
-		return new InMemoryUserDetailsManager(users);
-	}
-
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(kayttajaDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
 }
