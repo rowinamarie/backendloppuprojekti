09b772b8a43ad3a1002822f643d0b71c6462db5f
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-10 18:08:30 +0200

    korjattu onetomany yhteys

--- src/main/java/loppuprojekti24/loppuprojekti/domain/Retki.java
+++ src/main/java/loppuprojekti24/loppuprojekti/domain/Retki.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,7 +29,7 @@ public class Retki {
     @JoinColumn(name = "kaupunki_id")
     private Kaupunki kaupunki;
 
-    @OneToMany(mappedBy = "retki")
+    @OneToMany(mappedBy = "retki", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private Set<Osallistuja> osallistujat = new HashSet<>();
 
