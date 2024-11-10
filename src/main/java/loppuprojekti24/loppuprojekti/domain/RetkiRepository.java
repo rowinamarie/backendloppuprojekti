b66cbd6c68d66ad6be6dc4077a839ba60cff4f4a
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-10 18:54:08 +0200

    lisätty findbyId

--- src/main/java/loppuprojekti24/loppuprojekti/domain/RetkiRepository.java
+++ src/main/java/loppuprojekti24/loppuprojekti/domain/RetkiRepository.java
@@ -1,11 +1,14 @@
 package loppuprojekti24.loppuprojekti.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface RetkiRepository extends CrudRepository <Retki, Long> {
 
     List <Retki> findByRetkinimi (String retkinimi);
+
+    Optional<Retki> findById(Long id);
 
 }
