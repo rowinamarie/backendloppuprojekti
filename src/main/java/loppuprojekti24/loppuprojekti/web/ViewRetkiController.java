2f0e5f336c03149d5d36fe2b41d2df950220785d
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-10 15:06:25 +0200

    lisätty view retkille

--- src/main/java/loppuprojekti24/loppuprojekti/web/ViewRetkiController.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/ViewRetkiController.java
@@ -1,12 +1,27 @@
 package loppuprojekti24.loppuprojekti.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 // Import required classes and packages
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import loppuprojekti24.loppuprojekti.domain.RetkiRepository;
 
 
 // Mark this class as a Controller to handle HTTP requests and views
 @Controller
 public class ViewRetkiController {
+
+    @Autowired
+    private RetkiRepository retkiRepository;
+
+
+        @RequestMapping(value= {"/", "/retket"})
+    public String studentList(Model model) {	
+        model.addAttribute("retket", retkiRepository.findAll());
+        return "retkilista";
+    }
 
     // viimeinen sulku
 }
