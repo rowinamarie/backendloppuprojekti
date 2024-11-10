58a705300257221f6d82b75a50d3cd4cbc11f904
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-10 17:51:28 +0200

    lisätty päivämäärä -muuttuja

--- src/main/java/loppuprojekti24/loppuprojekti/domain/Retki.java
+++ src/main/java/loppuprojekti24/loppuprojekti/domain/Retki.java
@@ -1,5 +1,6 @@
 package loppuprojekti24.loppuprojekti.domain;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,59 +18,51 @@ import jakarta.persistence.OneToMany;
 public class Retki {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long retkiId;
     private String retkinimi;
     private String kuvaus;
-
+    private LocalDate paivamaara;
 
     @ManyToOne
     @JoinColumn(name = "kaupunki_id")
     private Kaupunki kaupunki;
 
-   @OneToMany(mappedBy = "retki")
-    @JsonManagedReference 
+    @OneToMany(mappedBy = "retki")
+    @JsonManagedReference
     private Set<Osallistuja> osallistujat = new HashSet<>();
 
-
-
-    public Retki () {
+    public Retki() {
 
     }
 
-
-    public Retki(String retkinimi, String kuvaus, Kaupunki kaupunki, Set <Osallistuja> osallistujat) {
+    public Retki(String retkinimi, String kuvaus, Kaupunki kaupunki, LocalDate paivamaara, Set<Osallistuja> osallistujat) {
         this.retkinimi = retkinimi;
         this.kuvaus = kuvaus;
         this.kaupunki = kaupunki;
         this.osallistujat = osallistujat;
+        this.paivamaara = paivamaara;
     }
-
 
     public Long getRetkiId() {
         return retkiId;
     }
 
-
     public void setRetkiId(Long retkiId) {
         this.retkiId = retkiId;
     }
-
 
     public String getRetkinimi() {
         return retkinimi;
     }
 
-
     public void setRetkinimi(String retkinimi) {
         this.retkinimi = retkinimi;
     }
 
-
     public String getKuvaus() {
         return kuvaus;
     }
-
 
     public void setKuvaus(String kuvaus) {
         this.kuvaus = kuvaus;
@@ -79,29 +72,33 @@ public class Retki {
         return kaupunki;
     }
 
-
     public void setKaupunki(Kaupunki kaupunki) {
         this.kaupunki = kaupunki;
     }
 
+    public LocalDate getPaivamaara() {
+        return paivamaara;
+    }
+    public void setPaivamaara(LocalDate paivamaara) {
+        this.paivamaara = paivamaara;
+    }
 
-  public Set<Osallistuja> getOsallistujat() {
+    public Set<Osallistuja> getOsallistujat() {
         return osallistujat;
     }
 
-
     public void setOsallistujat(Set<Osallistuja> osallistujat) {
         this.osallistujat = osallistujat;
-    } 
-
+    }
 
     @Override
     public String toString() {
-        return "Retki [retkiId=" + retkiId + ", retkinimi=" + retkinimi + ", kuvaus=" + kuvaus + ", kaupunki=" + kaupunki + "]";
+        return "Retki [retkiId=" + retkiId + ", retkinimi=" + retkinimi + ", kuvaus=" + kuvaus + ", paivamaara="
+                + paivamaara + ", kaupunki=" + kaupunki + ", osallistujat=" + osallistujat + "]";
     }
+
 
     
 
-
-    //viimeinen sulku
+    // viimeinen sulku
 }
