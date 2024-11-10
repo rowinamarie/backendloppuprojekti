65a5686d03af6f643ef9476e07fb1bfda2f1bfd7
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-10 17:47:49 +0200

    poistettu turhat importit

--- src/main/java/loppuprojekti24/loppuprojekti/domain/Kaupunki.java
+++ src/main/java/loppuprojekti24/loppuprojekti/domain/Kaupunki.java
@@ -1,9 +1,6 @@
 package loppuprojekti24.loppuprojekti.domain;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
