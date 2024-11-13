24d15d05ffffe6e7012a92bc3eec0c221b3e7b19
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-13 10:18:12 +0200

    poistettu in memory käyttäjät ja tuotu UserServiceDetail

--- src/main/java/loppuprojekti24/loppuprojekti/web/WebSecurityConfig.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/WebSecurityConfig.java
@@ -6,21 +6,21 @@ import java.util.List;
 //antMatcherin importointi
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
+
+	@Autowired
+	private UserDetailServiceImpl userDetailsService;
 
     @Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -36,24 +36,9 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		
-		System.out.println("in-memory users - luodaan näitä kaksi kappaletta");
-		List<UserDetails> users = new ArrayList<UserDetails>();
-
-		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-		UserDetails user1 = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
-
-		users.add(user1);
-
-		UserDetails user2 = User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER", "ADMIN")
-				.build();
-
-		users.add(user2);
-
-		return new InMemoryUserDetailsManager(users);
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
 
