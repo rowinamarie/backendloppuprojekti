54867ccd95fd7a0f48a9297ed7a8da90c295e186
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-14 21:03:24 +0200

    lisätty autentikointi

--- src/main/java/loppuprojekti24/loppuprojekti/web/RetkiControllerRest.java
+++ src/main/java/loppuprojekti24/loppuprojekti/web/RetkiControllerRest.java
@@ -3,9 +3,11 @@ package loppuprojekti24.loppuprojekti.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import loppuprojekti24.loppuprojekti.domain.Retki;
@@ -14,22 +16,26 @@ import loppuprojekti24.loppuprojekti.domain.RetkiRepository;
 
 
 @RestController
+@RequestMapping("/trips")
 public class RetkiControllerRest {
 
     @Autowired
     private RetkiRepository retkiRepository;
 
-        // GET endpoint kaupungin hakemiseen
-    @GetMapping("/trips")
+    // GET endpoint kaupungin hakemiseen
+    @PreAuthorize ("hasAuthority('OPETTAJA')")
+    @GetMapping
     public List<Retki> getAllRetket() {
         return (List<Retki>) retkiRepository.findAll();
     }
 
     // POST endpoint kaupungin lisäämiseen
-    @PostMapping ("/trips")
+    @PostMapping ("")
     public Retki addRetki(@RequestBody Retki retki) {
         return retkiRepository.save(retki);
     }
+
+    
 
 
 }
