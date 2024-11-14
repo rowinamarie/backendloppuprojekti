4abf8ee82f5ac8b63ace1940bc4b8e75a87c9422
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-14 21:22:15 +0200

    poistettu turhat importit

--- src/main/java/loppuprojekti24/loppuprojekti/domain/Retki.java
+++ src/main/java/loppuprojekti24/loppuprojekti/domain/Retki.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.NotEmpty;
 
 @Entity
