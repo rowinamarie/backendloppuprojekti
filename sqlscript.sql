45d13d87749f3f8697f6b9049911aacc1861eb1a
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-14 17:57:26 +0200

    lisätty testidataa

--- sqlscript.sql
+++ sqlscript.sql
@@ -33,3 +33,22 @@ CREATE TABLE Osallistuja (
 );
 
 
+-- Lisätään testidata tauluun Kaupunki
+INSERT INTO Kaupunki (kaupunkinimi) VALUES ('Saimaa');
+INSERT INTO Kaupunki (kaupunkinimi) VALUES ('Inari');
+INSERT INTO Kaupunki (kaupunkinimi) VALUES ('Turku');
+
+-- Lisätään testidata tauluun Retki
+INSERT INTO Retki (retkinimi, kuvaus, paivamaara, kaupunki_id) VALUES 
+('Kesäretki Saimaalle', 'Kolmen päivän retki.', '2024-06-15', 1), -- kaupunki_id 1 (Saimaa)
+('Ruskavaellus Lapissa', 'Nukkumista tähtitaivaan alla.', '2024-09-08', 2); -- kaupunki_id 2 (Inari)
+
+-- Lisätään testidata tauluun Osallistuja
+INSERT INTO Osallistuja (etunimi, sukunimi, sahkoposti, retki_id) VALUES 
+('Matti', 'Meikaläinen', 'matti.meikalainen@sahkoposti.fi', 1), -- retki_id 1 (Kesäretki Saimaalle)
+('Liisa', 'Korhonen', 'liisa@sahkposti.fi', 2); -- retki_id 2 (Ruskavaellus Lapissa)
+
+-- Lisätään testidata tauluun Kayttaja
+INSERT INTO kayttajat (kayttajatunnus, salasana, role) VALUES 
+('Oppilas', '$2a$12$xb2011I0iidDDSNtDbmQGOPGkSKfePo2gCmB7T.77r.HrYAlw4W.C', 'OPPILAS'),
+('Opettaja', '$2a$12$gNMXsAo29KleVf86UVrB1.4lPBiPN454rd3X0gYBMVy9AePtEhAXK', 'OPETTAJA');
